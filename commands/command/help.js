9731c5ee0216f6d64ae1c58251f566ee3aed9a9f
Tolfx <matteus12122@gmail.com>
2020-09-14 21:57:38 +0200

    Comments

--- commands/command/help.js
+++ commands/command/help.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: "help",
-  aliases: ["h"],
-  description: "Returns all commands, or one specific command info",
-  run: async (client, message, args) => {
+  name: "help", // <-- Always needed, the name will be used for the bot to find the command like !help.
+  aliases: ["h"], //If you want to make it shorter
+  description: "Returns all commands, or one specific command info", // When someone is reading this they can understand ig
+  run: async (client, message, args) => { // <-- Always needed, otherwise it wont run correctly.
     message.channel.send("This is a help command?");
   },
 };

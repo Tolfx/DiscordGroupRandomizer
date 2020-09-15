1755b800d735883f95b7abe0331986f2c13c4430
Tolfx <matteus12122@gmail.com>
2020-09-15 14:01:34 +0200

    🎇Example for < createChannel => sort.js >

--- commands/command/sort.js
+++ commands/command/sort.js
@@ -1,14 +1,26 @@
 const { readMembersVoice } = require("../../lib/findMembers");
+const { createChannel } = require("../../lib/manageChannels");
 
 module.exports = {
   name: "sort",
   aliases: ["s"],
   description: "Sorts something",
   run: async (client, message, args) => {
+    const roles = [
+      {
+        name: "test1",
+        id: "670167467552800778",
+      },
+      {
+        name: "test2",
+        id: "755386039572496414",
+      },
+    ];
     try {
       const data = await readMembersVoice(message);
 
       message.channel.send(`ID: ${data.channelID} \nAmount: ${data.members.length}`);
+      createChannel(message, roles);
     } catch (err) {
       message.channel.send(err);
     }

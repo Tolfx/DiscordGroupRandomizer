c8da9423373c1daba99cde5b6ceadda364b502c7
Tolfx <matteus12122@gmail.com>
2020-09-18 11:01:48 +0200

    ✨Modified < clear.js & manageChannels.js & manageServer.js >

--- commands/command/clear.js
+++ commands/command/clear.js
@@ -10,9 +10,17 @@ module.exports = {
   run: async (client, message, args) => {
     fs.readFile(`./data/${message.author.id}.json`, async (err, data) => {
       if (data) {
+        //Parse the object
         const information = JSON.parse(data);
+
+        //Remove the channels
         removeChannels(message, information.serverID);
+
+        //Remove the roles
         removeRoles(message, information.roleID);
+
+        //Delete the file.
+        fs.unlink(`./data/${message.author.id}.json`);
       } else {
         return message.channel.send(`Please use ${_config.prefix}sort first`);
       }

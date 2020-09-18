c4c07c2e67ff9de95281f6d4dff1c453671d9a7c
Tolfx <matteus12122@gmail.com>
2020-09-18 12:57:04 +0200

    ✨Modified < sort.js >

--- commands/command/sort.js
+++ commands/command/sort.js
@@ -1,4 +1,4 @@
-const { readMembersVoice } = require("../../lib/manageMembers");
+const { readMembersVoice, moveMember } = require("../../lib/manageMembers");
 const { createChannel } = require("../../lib/manageChannels");
 const { createRoles } = require("../../lib/manageServer");
 const _config = require("../../config.json");
@@ -9,6 +9,11 @@ module.exports = {
   aliases: ["s"],
   description: "Sorts something",
   run: async (client, message, args) => {
+    if (!args[0])
+      return message.channel.send("Please provide an amount of how many groups you want");
+
+    const amountOfGroups = parseInt(args[0]);
+
     try {
       //Stop!
       fs.readFile(`./data/${message.author.id}.json`, async (err, data) => {
@@ -17,15 +22,22 @@ module.exports = {
             `You've been using this command before, please use ${_config.prefix}clear first.`
           );
         } else {
-          let names = "Grupprum";
+          //The name of the channels
+          let names = `${message.author.username} Grupprum`;
+
+          //An array for the channels
           let nameArray = [];
+
+          //The data of the members on the voice channel.
           const data = await readMembersVoice(message);
 
-          for (let i = 0; i < data.members.length; ++i) {
+          //Creates the names kek
+          for (let i = 0; i < amountOfGroups; ++i) {
             nameArray.push((names += i + 1));
           }
 
-          await createRoles(message, data.members.length, nameArray).then(async (roles) => {
+          //Create the roles.
+          await createRoles(message, amountOfGroups, nameArray).then(async (roles) => {
             //Channel
             let server = await createChannel(message, roles);
 
@@ -54,6 +66,7 @@ module.exports = {
                 if (err) throw err;
               }
             );
+            moveMember(client, message, data.members[0].id, serverID[0]);
           });
         }
       });

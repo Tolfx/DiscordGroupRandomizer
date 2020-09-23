fa7b65ecde51fb094b4d99ebe3e7d968a8af697f
Tolfx <matteus12122@gmail.com>
2020-09-23 20:04:22 +0200

    🎈Modified < clear.js >

--- commands/command/clear.js
+++ commands/command/clear.js
@@ -1,27 +1,34 @@
-const fs = require("fs");
-const _config = require("../../config.json");
-const { removeChannels } = require("../../lib/manageChannels");
-const { removeRoles } = require("../../lib/manageServer");
+const fs = require('fs');
+const _config = require('../../config.json');
+const { removeChannels } = require('../../lib/manageChannels');
+const { removeRoles } = require('../../lib/manageServer');
+const { readMembersVoice, moveMember, giveRole } = require('../../lib/manageMembers');
 
 module.exports = {
-  name: "clear",
-  aliases: ["s"],
-  description: "Clears the users channels and roles and etc..",
+  name: 'clear',
+  aliases: ['s'],
+  description: 'Clears the users channels and roles and etc..',
   run: async (client, message, args) => {
     fs.readFile(`./data/${message.author.id}.json`, async (err, data) => {
       if (data) {
         //Parse the object
         const information = JSON.parse(data);
 
-        //Remove the channels
-        removeChannels(message, information.serverID).catch((err) => message.channel.send(err));
+        for (let i = 0; i < information.Members.length; ++i) {
+          moveMember(client, message, information.Members[i], information.authorChannel);
 
-        //Remove the roles
-        removeRoles(message, information.roleID).catch((err) => message.channel.send(err));
+          if (i + 1 === information.Members.length) {
+            //Remove the channels
+            removeChannels(message, information.serverID).catch((err) => message.channel.send(err));
+
+            //Remove the roles
+            removeRoles(message, information.roleID).catch((err) => message.channel.send(err));
+          }
+        }
 
         //Delete the file.
         fs.unlink(`./data/${message.author.id}.json`, (err) => {
-          if (err) return console.log("an error");
+          if (err) return console.log('an error');
         });
       } else {
         return message.channel.send(`Please use ${_config.prefix}sort first`);

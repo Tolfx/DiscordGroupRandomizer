0f060c98db3a9c290c0bcfdba8d5119633bf2846
Tolfx <matteus12122@gmail.com>
2020-10-03 21:34:01 +0200

    ✨Fixed < spread.js >

--- commands/command/spread.js
+++ commands/command/spread.js
@@ -12,12 +12,19 @@ module.exports = {
         //Parse the object
         const information = JSON.parse(data);
 
-        for (let i = 0; i < information.Members.length; ++i) {
-          moveMember(client, message, information.Members[i].Member, information.Members[i].Server);
+        let memberWhoLeft = []
 
-          if (i + 1 === information.Members.length) {
-            message.channel.send("Spread all of the members back according to their channels.");
-          }
+        for (let i = 0; i < information.Members.length; ++i) {
+          await moveMember(client, message, information.Members[i].Member, information.Members[i].Server, (err, data) => {
+            if (err) {
+              memberWhoLeft.push(information.Members[i].Member)
+              console.log(`User with ID: ${information.Members[i].Member} left`); 
+            }
+
+            if (i + 1 === information.Members.length) {
+              message.channel.send("Spread all of the members back according to their channels.");
+            };
+          })
         }
       } else {
         message.channel.send("You have nothing to spread..");
